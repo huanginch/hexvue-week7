e25348b83745fa14687e1de592a7e248dcd30859
huanginch <huanginch329@gmail.com>
2023-02-20 21:18:52 +0800

    Add base url to project
    The base url is the name of this repo, hexvue-week7

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/huanginch/hexvue-7.git master:gh-pages
+git push -f https://github.com/huanginch/hexvue-week7.git master:gh-pages
 
 cd -
